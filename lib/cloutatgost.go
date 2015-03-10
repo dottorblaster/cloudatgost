8bc03d2ed72395ee888c94dc35f5571664feab80
Alessio Biancalana <dottorblaster@gmail.com>
2015-03-10 23:55:15 +0100

    cloudatgost.go properly commented

--- lib/cloutatgost.go
+++ lib/cloutatgost.go
@@ -1,4 +1,4 @@
-// CloudAtCost API wrapper written in Go.
+// Package cloudatgost is a CloudAtCost API wrapper written in Go.
 package cloudatgost
 
 import (
@@ -6,6 +6,8 @@ import (
 	"net/http"
 )
 
+// A Client represents a CloudAtCost API client. It does HTTP requests
+// and returns properly populated structures.
 type Client struct {
 	client *http.Client
 	BaseURL string
@@ -13,6 +15,10 @@ type Client struct {
 	Token string
 }
 
+// NewClient instantiates and returns a new API client. This function
+// accepts an email and a token as its parameters, as well as an optional
+// HTTP client. If httpClient is nil, the function creates a new
+// http.DefaultClient and assigns it to the newly created client.
 func NewClient(email string, token string, httpClient *http.Client) *Client {
 	if httpClient == nil {
 		httpClient = http.DefaultClient
@@ -30,6 +36,9 @@ func NewClient(email string, token string, httpClient *http.Client) *Client {
 	return c
 }
 
+// Do is a shorthand for HTTP requests in the cloudatgost client domain.
+// The function takes an HTTP request, sends it and returns the API response,
+// decoded to the v, usually passed passed by reference.
 func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
 	resp, err := c.client.Do(req)
 	if err != nil {
